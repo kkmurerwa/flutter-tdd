1142f7c03cac2278b8bd6f64d86d2fc23f6c5f77
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-17 12:47:22 +0300

    feat: Add implementation and tests for NetworkInfo and NetworkInfoImpl classes

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter_tdd/core/network/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'network_info_test.mocks.dart';
+
+@GenerateNiceMocks([
+  MockSpec<InternetConnectionChecker>()
+])
+void main() {
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
+  late NetworkInfoImpl networkInfo;
+
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockInternetConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to InternetConnectionChecker.hasConnection', () async {
+      final tHasConnectionFuture = Future.value(true);
+      
+      when(mockInternetConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+
+      final result = networkInfo.isConnected;
+
+      verify(mockInternetConnectionChecker.hasConnection);
+      expect(result, equals(tHasConnectionFuture));
+    });
+  });
+
+}
